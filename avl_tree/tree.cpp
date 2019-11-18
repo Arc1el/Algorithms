64c14337765393a2580613a29545d4cb720d5e98
Arc1el <khm970514@gmail.com>
2019-11-18 21:28:19 +0900

    Update tree.cpp

--- avl_tree/tree.cpp
+++ avl_tree/tree.cpp
@@ -134,10 +134,10 @@ AVLNode* delete_node(AVLNode* root, int key)
                 root = NULL; 
             } 
             else //자식 1명
-			{
-				 *root = *temp;
-			}
-			free(temp); 
+            { 
+		*root = *temp;
+            }
+	free(temp); 
         } 
         else
         { 
