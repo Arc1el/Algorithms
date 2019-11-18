515b9838473bae36f067cc9507572f45e1bc1957
Arc1el <khm970514@gmail.com>
2019-11-18 21:14:10 +0900

    Update delete function

--- avl_tree/tree.cpp
+++ avl_tree/tree.cpp
@@ -5,13 +5,14 @@
 typedef struct AVLNode
 {
 	int key;
+	int height;
 	struct AVLNode *left;
 	struct AVLNode *right;
 } AVLNode;
 
 int get_height(AVLNode *node)
 {
-  int height = 0;
+	int height = 0;
 
 	if (node != NULL)
 		height = 1 + __max(get_height(node->left),
@@ -24,8 +25,7 @@ int get_balance(AVLNode* node)
 {
 	if (node == NULL) return 0;
 
-	return get_height(node->left)
-		- get_height(node->right);
+	return get_height(node->left) - get_height(node->right);
 }
 
 AVLNode* create_node(int key)
@@ -96,7 +96,96 @@ AVLNode* insert(AVLNode* node, int key)
 	return node;
 }
 
-// 전위 순회 함수
+AVLNode* minValueNode(AVLNode* node) 
+{ 
+    AVLNode* current = node; 
+
+    while (current->left != NULL) //단말노드 찾기
+        current = current->left; 
+  
+    return current; 
+} 
+
+AVLNode* delete_node(AVLNode* root, int key) 
+{   
+    if (root == NULL) 
+	{
+        return root; 
+	}
+ 
+    if (key < root->key) 
+	{
+        root->left = delete_node(root->left, key); 
+	}
+ 
+    else if(key > root->key) 
+	{
+        root->right = delete_node(root->right, key); 
+	}
+  
+    else
+    { 
+        if((root->left == NULL) || (root->right == NULL)) //자식이 1명만 있거나 없거나
+        { 
+            AVLNode *temp = root->left ? root->left : root->right;   
+            if (temp == NULL) //자식없음
+            { 
+                temp = root; 
+                root = NULL; 
+            } 
+            else //자식 1명
+			{
+				 *root = *temp;
+			}
+			free(temp); 
+        } 
+        else
+        { 
+            AVLNode* temp = minValueNode(root->right); 
+            root->key = temp->key; 
+            root->right = delete_node(root->right, temp->key); 
+        } 
+    } 
+
+    if (root == NULL) 
+	{
+		return root;
+	}
+  
+    root->height = 1 + __max(get_height(root->left), get_height(root->right)); 
+  
+    int balance = get_balance(root); 
+  
+    // LL
+    if (balance > 1 && get_balance(root->left) >= 0) 
+	{
+        return rotate_right(root); 
+	}
+  
+    // LR
+    if (balance > 1 && get_balance(root->left) < 0) 
+    { 
+        root->left =  rotate_left(root->left); 
+        return rotate_right(root); 
+    } 
+  
+    // RR
+    if (balance < -1 && get_balance(root->right) <= 0) 
+	{
+        return rotate_left(root); 
+	}
+  
+    // RL
+    if (balance < -1 && get_balance(root->right) > 0) 
+    { 
+        root->right = rotate_right(root->right); 
+        return rotate_left(root); 
+    } 
+  
+    return root; 
+} 
+
+// 전위순회
 void preorder(AVLNode *root)
 {
 	if (root != NULL)
@@ -106,22 +195,46 @@ void preorder(AVLNode *root)
 		preorder(root->right);
 	}
 }
+//중위순회
+void inorder(AVLNode* root)
+{
+	if(root)
+	{
+		inorder(root->left);
+		printf("[%d] ", root->key);
+		inorder(root->right);
+	}
+}
+//전위순회
+void postorder(AVLNode* root)
+{
+	if(root)
+	{
+		postorder(root->left);
+		postorder(root->right);
+		printf("[%d] ", root->key);
+	}
+}
 
 int main(void)
 {
 	AVLNode *root = NULL;
 
+	root = insert(root, 1);
+	root = insert(root, 4);
+	root = insert(root, 2);
 	root = insert(root, 5);
 	root = insert(root, 6);
-	root = insert(root, 7);
-	root = insert(root, 4);
 	root = insert(root, 3);
-	root = insert(root, 2);
-	root = insert(root, 1);
-	root = insert(root, 8);
-	root = insert(root, 9);
 
-	printf("전위 순회 결과 \n");
+	printf("삭제연산 전 전위순회 결과\n");
+	preorder(root);
+	printf("\n");
+
+	root = delete_node(root, 6);
+	root = delete_node(root, 5);
+
+	printf("\n삭제연산 후 전위 순회 결과 \n");
 	preorder(root);
 
 	return 0;
